2e96000fc160b046108b1285772d2df192c0bb48
1ilsang <1ilsang@naver.com>
2023-12-17 02:52:50 +0900

    feat: Add Macro with Function

--- README.md
+++ README.md
@@ -4,5 +4,7 @@
 
 0. [설치](./docs/install.md)
 1. [변수](./docs/variables.md)
+2. [함수](./docs/functions.md)
+3. [매크로](./docs/macro.md)
 
 TBD
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+fn add(a: i32, b: i32) -> i32 {
+  return a + b;
+}
+
 fn main() {
-    println!("Hello, world!");
+  let a = add(1, 2);
+  let mut b = 123;
+  b = 321;
+  println!("{:?}", b)
 }
